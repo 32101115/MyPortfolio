837368aa1eb62fb73efc75d97ad6524a6be4f56d
Jitae Kim <kimjt1226@gmail.com>
2019-10-06 22:11:39 -0400

    Updates

---
+++ precache-manifest.122facd8e0ee3d050ef2aad77c6fb0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a393731512524f1fbf4afd6912102a3e",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "d84649d99b77c62e6721",
+    "url": "/MyPortfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "96032bf90326c6d637ba",
+    "url": "/MyPortfolio/static/css/main.4a724c3d.chunk.css"
+  },
+  {
+    "revision": "d84649d99b77c62e6721",
+    "url": "/MyPortfolio/static/js/2.c1282ced.chunk.js"
+  },
+  {
+    "revision": "96032bf90326c6d637ba",
+    "url": "/MyPortfolio/static/js/main.f91e4cd6.chunk.js"
+  },
+  {
+    "revision": "d5f32359310bcebd5fb6",
+    "url": "/MyPortfolio/static/js/runtime~main.5d3b1208.js"
+  }
+]);
